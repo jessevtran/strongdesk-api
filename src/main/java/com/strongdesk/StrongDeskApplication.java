4a16f29cd63a17448a7a73139310847eddf847ef
Jesse Tran <jtran58@gmu.edu>
2025-07-23 02:09:35 -0400

    Init commit

---
+++ src/main/java/com/strongdesk/StrongDeskApplication.java
@@ -0,0 +1,13 @@
+package com.strongdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrongDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strongdesk/StrongDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strongdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrongDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
